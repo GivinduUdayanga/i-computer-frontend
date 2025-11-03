9ff04d9c904e0d03a68edaea62ea203bd60defe7
GivinduUdayanga <givindurajapaksha@gmail.com>
2025-11-03 15:03:22 +0530

    Day-11  👻

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import { ProductCard } from './components/productCard'
+
+
 
 
 function App() {
   
 
   return (
-    <>
-      <ProductCard name = "Laptop" price= "100,000.00" image= "https://picsum.photos/id/0/5000/3333"/>
-      <ProductCard name = "Smartphone" price= "60,000.00" image= "https://picsum.photos/id/1/5000/3333"/>
-      <ProductCard name = "Tablet" price= "30,000.00" image= "https://picsum.photos/id/2/5000/3333"/>
+    <div className="border w-[600px] h-[600px] bg-gray-400 relative">
+      <div className="w-[500px] h-[500px] bg-yellow-100 flex flex-col justify-center items-center ">
+        <div className="w-[100px] h-[100px] bg-blue-600">
 
-    </>
+        </div>
+        <div className="w-[100px] h-[100px] bg-red-600 fixed left-[550px] top-[550px]">
+
+        </div>
+        <div className="w-[100px] h-[100px] bg-green-600">
+
+        </div>
+        <div className="w-[100px] h-[100px] bg-pink-600 absolute right-[100px] bottom-[100px]">
+
+        </div>
+      </div>
+
+    </div>
   )
 }
 
